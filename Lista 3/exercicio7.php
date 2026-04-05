7d25c07b6a646cf463e219d649f9af01f6960f95
ramosjean <ramosjean233@gmail.com>
2026-04-05 08:40:33 -0300

    exeercicio 7 foi usado  o while para mostrar no crescente e no 8 para descrecente não usei o do e deu certo

---
+++ Lista 3/exercicio7.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 7</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 7</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="numero" class="form-label">Digite um numero para mostrar em ordem </label>
+              <input type="text" id="numero" name="numero" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $numero = $_POST['numero'];
+    $comeco = 1;
+
+    while($comeco <=$numero){
+        echo "Número: $comeco<br>";
+        $comeco++;
+    }
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
