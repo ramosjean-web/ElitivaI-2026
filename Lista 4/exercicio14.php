9e30aeef009347daeeba6e50c37c0711f328a72f
ramosjean <ramosjean233@gmail.com>
2026-04-07 15:08:46 -0300

    Foi usado três funções para fazer essa atividade

--- Lista 4/exercicio14.php
+++ Lista 4/exercicio14.php
@@ -3,31 +3,29 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 13 </title>
+<title>Exercício 2 Lista 4</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercício 13 </h1>
+<h1>Exercício 2 Lista 4</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="palavra" class="form-label">Digite uma frase</label>
-              <input type="text" id="frase" name="frase" class="form-control" required="">
+              <label for="palavra" class="form-label">Digite uma palavra    </label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
-    <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mapa = strtolower(trim($_POST["palavra"]));
-    $invertida = strrev($mapa);
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+     $mapa = $_POST["palavra"];
+    
 
-    if ($mapa == $invertida) {
-        echo "<br> " . $mapa . " → é palíndromo";
-    } else {
-        echo "<br> " . $mapa . " → não é";
-    }
+    echo "<br> MAIÚSCULA: " .strtoupper($mapa);
+    echo "<br> minuscula: " .strtolower($mapa);
+       
 }
-?>
+?>	
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
