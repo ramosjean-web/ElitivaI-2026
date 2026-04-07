966285928bb67c062a4d1be023666ba8542b3f71
ramosjean <ramosjean233@gmail.com>
2026-04-07 15:24:34 -0300

    Foi finalizado com explore e os ultimos dois foi dado um comitt mas acabei me confundindo

--- Lista 4/exercico15.php
+++ Lista 4/exercico15.php
@@ -3,29 +3,31 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 13 </title>
+<title>Exercicio 15</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercício 13 </h1>
+<h1>Exercicio 15</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="palavra" class="form-label">Digite uma frase</label>
-              <input type="text" id="frase" name="frase" class="form-control" required="">
+              <label for="email" class="form-label">Digite o email</label>
+              <input type="text" id="email" name="email" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mapa = $_POST["frase"];
+    $mapa = trim($_POST["email"]);
+    $partes = explode("@", $mapa);
 
-    echo "<br> Frase original: " . $mapa;
-    echo "<br> Sem espaços no início e final: " . trim($mapa);
-    echo "<br> Sem nenhum espaço: " . str_replace(" ", "", $mapa);
+    echo "<br> Domínio: " . $partes[1];
 }
 ?>
+
+</body>
+</html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
-</html> 
+</html>
--- Lista 4/exercio14.php
+++ Lista 4/exercio14.php
@@ -3,29 +3,32 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 13 </title>
+<title>Exercício 14 </title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercício 13 </h1>
+<h1>Exercício 14</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="palavra" class="form-label">Digite uma frase</label>
-              <input type="text" id="frase" name="frase" class="form-control" required="">
+              <label for="palavra" class="form-label">Digite uma Palavra</label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mapa = $_POST["frase"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mapa = strtolower(trim($_POST["palavra"]));
+    $invertida = strrev($mapa);
 
-    echo "<br> Frase original: " . $mapa;
-    echo "<br> Sem espaços no início e final: " . trim($mapa);
-    echo "<br> Sem nenhum espaço: " . str_replace(" ", "", $mapa);
+    if ($mapa == $invertida) {
+        echo "<br> " . $mapa . " → é palíndromo";
+    } else {
+        echo "<br> " . $mapa . " → não é";
+    }
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
-</html> 
+</html>
